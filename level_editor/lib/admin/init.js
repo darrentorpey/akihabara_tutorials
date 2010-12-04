373dae455dca38ef2286cd790906ed0eea9c31fa
Darren Torpey <dct@punchbowlsoftware.com>
2010-12-03 22:41:19 -0500

    Toggle box for auto-update

--- level_editor/lib/admin/init.js
+++ level_editor/lib/admin/init.js
@@ -1,5 +1,13 @@
-drawGuiActions([
-  {
-    'reload map': 'redrawMap()'
+drawGuiActions();
+
+$('body').mouseup(function() {
+  if ($('#toggle_autoupdate input:checked').length) {
+    redrawMap();
   }
-]);
+});
+
+$('<label id="toggle_autoupdate"><input type="checkbox" />auto-update</label>').appendTo('#admin_buttons').find('input').change(function() {
+  if ($(this).attr('checked')) {
+    redrawMap();
+  }
+});

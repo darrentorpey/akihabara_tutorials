86ca4797663e2c4ed5b8ea364d45e3708557ac33
Darius Kazemi <darius.kazemi@gmail.com>
2010-12-15 09:49:45 -0500

    Added new audio channel for explosion

--- level_editor/resources/bundle.js
+++ level_editor/resources/bundle.js
@@ -76,7 +76,7 @@
 		["jump",["resources/jump.mp3","resources/jump.ogg"],{channel:"jump"}],	
     ["hit",["resources/hit.mp3","resources/hit.ogg"],{channel:"hit"}],
     ["squish",["resources/sword.mp3","resources/sword.ogg"],{channel:"hit"}],
-    ["explode",["resources/megaexplosion.mp3","resources/megaexplosion.ogg"],{channel:"hit"}],
+    ["explode",["resources/megaexplosion.mp3","resources/megaexplosion.ogg"],{channel:"boom"}],
     ["star",["resources/coin.mp3","resources/coin.ogg"],{channel:"hit"}]
 	]
 }

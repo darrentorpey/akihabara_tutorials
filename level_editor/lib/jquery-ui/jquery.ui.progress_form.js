c110a76309e0099bcea8c1caad06b5492dbd904b
Darren Torpey <dct@punchbowlsoftware.com>
2010-12-24 00:33:49 -0500

    Some tweaks for the example tutorial

--- level_editor/lib/jquery-ui/jquery.ui.progress_form.js
+++ level_editor/lib/jquery-ui/jquery.ui.progress_form.js
@@ -1,11 +1,12 @@
 var ProgressForm = {
 
-  _currentStep: 1,
-  _totalSteps:  0,
+  _currentStep:   1,
+  _totalSteps:    0,
 
-  _prevButton:  false,
-  _nextButton:  false,
-  _progressbar: false,
+  _prevButton:    false,
+  _nextButton:    false,
+  _restartButton: false,
+  _progressbar:   false,
 
   // this would be _init() for jQuery UI versions < 1.8
   _create: function() {
@@ -14,11 +15,13 @@ var ProgressForm = {
 
     this._totalSteps = this.element.find('section').length;
 
-    this._prevButton = $('<input type="button" value="previous step">');
-    this._nextButton = $('<input type="button" value="next step">');
+    this._prevButton    = $('<input type="button" value="previous step">');
+    this._nextButton    = $('<input type="button" value="next step">');
+    this._restartButton = $('<input type="button" value="restart">');
 
-    this.element.append(this._prevButton).append(' ').append(this._nextButton);
+    $('<div class="buttons"></div>').appendTo(this.element).append(this._prevButton).append(' ').append(this._nextButton).append(' &nbsp; ').append(this._restartButton);
     this._prevButton.hide();
+    this._restartButton.hide();
 
     this._progressbar = $('<div id="progress"/>').prependTo(this.element);
     this._progressbar.progressbar({ value: ((100/this._totalSteps) * this._currentStep) });
@@ -28,11 +31,13 @@ var ProgressForm = {
     var tmp = this;
     this._nextButton.click(function() { tmp.nextStep(); });
     this._prevButton.click(function() { tmp.prevStep(); });
+    this._restartButton.click(function() { tmp.restart(); });
   },
 
   _update_state: function() {
     this._prevButton.toggle(this._currentStep > 1);
     this._nextButton.toggle(this._currentStep < this._totalSteps);
+    this._restartButton.toggle(this._currentStep > 2);
     this.element.find('input[type=submit]').toggle(this._currentStep == this._totalSteps);
     this._progressbar.progressbar('option', 'value', ((100/this._totalSteps) * this._currentStep));
     this._progressbar.find('.ui-progressbar-value').text('Step ' + this._currentStep + ' of '+ this._totalSteps);
@@ -47,6 +52,13 @@ var ProgressForm = {
   prevStep: function() {
     this.element.find('section:visible').hide().prev().show();
     this._currentStep -= 1;
+    this._update_state();
+  },
+
+  restart: function() {
+    this.element.find('section').show();
+    this.element.find('section:not(:first)').hide();
+    this._currentStep = 1;
     this._update_state();
   }
 };

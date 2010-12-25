bf44ef76dde978f1189ef5e022041a8f87af3b98
Darren Torpey <dct@punchbowlsoftware.com>
2010-12-25 12:09:00 -0500

    Small example tutorial improvements

--- level_editor/lib/jquery-ui/jquery.ui.progress_form.js
+++ level_editor/lib/jquery-ui/jquery.ui.progress_form.js
@@ -19,19 +19,46 @@ var ProgressForm = {
     this._nextButton    = $('<input type="button" value="next step">');
     this._restartButton = $('<input type="button" value="restart">');
 
-    $('<div class="buttons"></div>').appendTo(this.element).append(this._prevButton).append(' ').append(this._nextButton).append(' &nbsp; ').append(this._restartButton);
+    $('<div class="buttons"></div>').css('padding-top', '5px').appendTo(this.element).append(this._prevButton).append(this._nextButton).append(this._restartButton);
     this._prevButton.hide();
     this._restartButton.hide();
 
     this._progressbar = $('<div id="progress"/>').prependTo(this.element);
     this._progressbar.progressbar({ value: ((100/this._totalSteps) * this._currentStep) });
 
-    this._progressbar.find('.ui-progressbar-value').text('Step ' + this._currentStep + ' of ' + this._totalSteps).css({ 'text-align':  'right', 'padding': '0.5em 0.5em 0 0' });
+    this._progressbar.find('.ui-progressbar-value').html('<span>Step ' + this._currentStep + ' of ' + this._totalSteps + '</span>');
 
     var tmp = this;
     this._nextButton.click(function() { tmp.nextStep(); });
     this._prevButton.click(function() { tmp.prevStep(); });
     this._restartButton.click(function() { tmp.restart(); });
+
+    this._init_css();
+  },
+
+  _init_css: function() {
+    this._progressbar.find('.ui-progressbar-value').css({
+      'text-align': 'right'
+    });
+
+    this._progressbar.find('.ui-progressbar-value span').css({
+      'display': 'inline-block',
+      'margin-top': '0.4em',
+      'margin-right': '0.5em'
+    });
+
+    // this._prevButton.css({
+    //   'background-image': 'url(../css/images/ui-icons_ef8c08_256x240.png)'
+    // });
+    // this._prevButton.addClass('ui-icon-carat-1-w');
+
+    this._nextButton.css({
+      float: 'right'
+    });
+
+    this._restartButton.css({
+      'margin-left': '10px'
+    });
   },
 
   _update_state: function() {
@@ -40,7 +67,7 @@ var ProgressForm = {
     this._restartButton.toggle(this._currentStep > 2);
     this.element.find('input[type=submit]').toggle(this._currentStep == this._totalSteps);
     this._progressbar.progressbar('option', 'value', ((100/this._totalSteps) * this._currentStep));
-    this._progressbar.find('.ui-progressbar-value').text('Step ' + this._currentStep + ' of '+ this._totalSteps);
+    this._progressbar.find('.ui-progressbar-value span').text('Step ' + this._currentStep + ' of '+ this._totalSteps);
   },
 
   nextStep: function () {

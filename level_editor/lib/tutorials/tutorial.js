4920e557027857e67e2c2565817e7487e78d7e20
Darren Torpey <dct@punchbowlsoftware.com>
2010-12-26 15:59:29 -0500

    Refactored tutorial definition into its own class

--- level_editor/lib/tutorials/tutorial.js
+++ level_editor/lib/tutorials/tutorial.js
@@ -1,5 +1,12 @@
 var Tutorial = Klass.extend({
-  init: function() {
-    
+  init: function(options) {
+    this.header = options.header;
+    this.steps = options.steps;
+  },
+
+  createDOM: function() {
+    return $('<div></div>').append('<header>' + this.header + '</header').append($.tmpl('tut_part', this.steps)).html();
   }
 });
+
+$.template('tut_part', '<section data-tut-part="${id}"><header>${header}</header>{{html content}}</section>');
--- level_editor/lib/tutorials/tutorial_manager.js
+++ level_editor/lib/tutorials/tutorial_manager.js
@@ -3,19 +3,11 @@ var TutorialManager = Klass.extend({
     this.tutorials = [];
   },
 
-  createTutorial: function(element, tut_definition) {
-    $(element).append('<header>' + tut_definition.header + '</header').append($.tmpl('tut_part', tut_definition.steps)).progressform();
+  createTutorial: function(element, tutorial) {
+    $(element).append(tutorial.createDOM()).progressform();
   }
 });
 var Tutorials = {};
 Tutorials.manager = new TutorialManager();
-
-var tut_parts = [
-  { id: 1, header: 'Getting started', content: '<p>Welcome!...</p>' },
-  { id: 2, header: 'Fundamentals', content: '<p>Do some stuff...</p>' },
-  { id: 3, header: 'Things to remember', content: '<p>Finally...</p>' }
-];
-
-$.template('tut_part', '<section data-tut-part="${id}"><header>${header}</header>{{html content}}</section>');
 
 // jQuery hook

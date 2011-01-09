e4bb7976535845a718d0a58248b0fd9e468b107f
dariusk <darius.kazemi@gmail.com>
2011-01-08 20:19:53 -0500

    Turned admin back on in defaults

--- level_editor/lib/config.js
+++ level_editor/lib/config.js
@@ -1,7 +1,7 @@
 $config = {
   flash_okay:                 false,
   save_and_load:              false,
-  use_admin_box:              false,
+  use_admin_box:              true,
   show_event_callout:         false,
   mouse_over_scrolling:       false,
   auto_cache_break_libraries: false
